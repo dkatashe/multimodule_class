11c5d036f3369f8cbab5e27da9636434140c7eb6
dkatashe <danil.katashev@gmail.com>
2019-03-14 18:48:49 +0300

    Command class

--- debt_collector/src/com/tsystems/oop/debtcollector/Application.java
+++ debt_collector/src/com/tsystems/oop/debtcollector/Application.java
@@ -12,40 +12,71 @@ public class Application
 {
   ConsoleUi ui;
   CsvLoader loader;
-  DbUtil dbUtil = DbUtil.getInstance();
+  DbUtil dbUtil=DbUtil.getInstance();
   Caller caller;
+
   public static void main(String[] args)
   {
     Application app=new Application();
     app.initApp();
+    app.run();
   }
 
   private void initApp()
   {
-    //your code
+    ui=new ConsoleUi();
   }
 
+  private void run()
+  {
+    boolean isRunning=true;
+
+    while (isRunning)
+    {
+      ConsoleUi.Command com=ui.getNextCommand();
+      if (com != null)
+      {
+        // Temporary code below
+        switch (com.type)
+        {
+          case LOAD:
+            System.out.print(com.type);
+            System.out.println(" " + com.arg);
+            break;
+          case GET_ALL:
+          case START_CALL:
+            System.out.println(com.type);
+            break;
+          case EXIT:
+            isRunning=false;
+            break;
+        }
+      }
+    }
+  }
 
   private void startCall()
   {
-    List<Map<String, String>> dataFromDb = dbUtil.getAll();
-    dataFromDb.forEach(debtor->{
-      String name = debtor.get("name");
-      String lastname = debtor.get("last_name");
-      String debt = debtor.get("debt");
-      String phone = debtor.get("phone");
-      if (caller.makeCallTo(name, lastname, phone, debt)){
+    List<Map<String, String>> dataFromDb=dbUtil.getAll();
+    dataFromDb.forEach(debtor -> {
+      String name=debtor.get("name");
+      String lastname=debtor.get("last_name");
+      String debt=debtor.get("debt");
+      String phone=debtor.get("phone");
+      if (caller.makeCallTo(name, lastname, phone, debt))
+      {
         debtor.replace("debt", "0");
       }
     });
 
-    dataFromDb.forEach(e->{
-      if (e.get("debt").equals("0")){
+    dataFromDb.forEach(e -> {
+      if (e.get("debt").equals("0"))
+      {
         dbUtil.deleteById(Integer.parseInt(e.get("id")));
       }
     });
   }
 
-// a lot of your code
+  // a lot of your code
 
 }
--- debt_collector/src/com/tsystems/oop/debtcollector/ConsoleUi.java
+++ debt_collector/src/com/tsystems/oop/debtcollector/ConsoleUi.java
@@ -5,18 +5,72 @@ import java.util.Scanner;
 public class ConsoleUi
 {
   Scanner sc;
+
   public ConsoleUi()
   {
+    greetUser();
     sc=new Scanner(System.in);
   }
 
-    private String getCommandFromConsole()
+  private String[] getCommandFromConsole()
   {
     String command=sc.nextLine();
+
+    return command.split(" ");
+  }
+
+  public Command getNextCommand()
+  {
+    Command command=null;
+    System.out.println("Please enter a command:");
+    String[] input=getCommandFromConsole();
+
+    try
+    {
+      if (input.length > 0)
+      {
+        switch (input[0].toLowerCase())
+        {
+          case "load":
+            if (input.length == 2)
+            {
+              command=new Command(input[0].toLowerCase(), input[1]);
+            }
+            else
+            {
+              throw new CommandParseException("Argument \"filepath\" is empty or invalid");
+            }
+            break;
+          case "startcall":
+          case "getall":
+          case "exit":
+            if (input.length == 1)
+            {
+              command=new Command(input[0].toLowerCase());
+            }
+            else
+            {
+              throw new CommandParseException("No arguments are acceptable for the command \"" + input[0] + "\"");
+            }
+            break;
+          default:
+            throw new CommandParseException("Unknown command \"" + input[0] + "\"");
+        }
+      }
+      else
+      {
+        throw new CommandParseException("No command entered");
+      }
+    }
+    catch (CommandParseException e)
+    {
+      e.printStackTrace();
+    }
+
     return command;
   }
 
-  public void greetUser()
+  private void greetUser()
   {
     System.out.println("Hello, Program DebtCollector is running");
     System.out.println("Available commands: ");
@@ -24,5 +78,55 @@ public class ConsoleUi
     System.out.println("startCall         - to start to call debtors");
     System.out.println("getAll            - to get all debtors");
     System.out.println("exit              - to exit");
+  }
+
+  public class Command
+  {
+    CommandType type;
+    String arg;
+
+    Command(String com)
+    {
+      type=determineType(com);
+    }
+
+    Command(String com, String arg)
+    {
+      type=determineType(com);
+      this.arg=arg;
+    }
+
+    private CommandType determineType(String type)
+    {
+      switch (type)
+      {
+        case "load":
+          return CommandType.LOAD;
+        case "startcall":
+          return CommandType.START_CALL;
+        case "getall":
+          return CommandType.GET_ALL;
+        case "exit":
+          return CommandType.EXIT;
+        default:
+          return null;
+      }
+    }
+  }
+
+  public enum CommandType
+  {
+    LOAD,
+    START_CALL,
+    GET_ALL,
+    EXIT
+  }
+
+  public class CommandParseException extends Exception
+  {
+    public CommandParseException(String message)
+    {
+      super(message);
+    }
   }
 }
